8cf9d6d948c1802bedab686366bf2be214eb8d49
avarghese <avarghese@SUYCOKHWDSK208>
2022-03-31 17:24:37 +0530

    Add project files.

---
+++ MobiZone/Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using BusinessObjectLayer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiLayer.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly ProductDbContext _context;
+        IAdminOperations _admin;
+        public AdminController(ProductDbContext context, IAdminOperations admin)
+        {
+            _context = context;
+            _admin = admin;
+
+        }
+        public IActionResult Get()
+        {
+            return new JsonResult("success");
+        }
+    }
+}
